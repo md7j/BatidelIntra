7e33755d610d3a5b9da616124c9c2759eaa56aad
abu <maxime.bures@epitech.eu>
2021-01-15 12:21:32 +0100

    Started planning

--- BatidelIntra/planning/urls.py
+++ BatidelIntra/planning/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name = 'planning'
 urlpatterns = [
     path('', views.planning, name='planning'),
+    path('coords/', views.coords, name='coords'),
 ]
--- BatidelIntra/planning/views.py
+++ BatidelIntra/planning/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
-
 
 @login_required
 def planning(request):
     return render(request, 'planning/planning.html')
+
+def coords(request):
+    data = {
+        'name': request.body.GET.get(),
+        'location': 'Finland',
+        'is_active': True,
+        'count': 28
+    }
+    return JsonResponse(data)
